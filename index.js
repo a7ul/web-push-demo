e1e245457c97e151983c129f0323f1bb7fb7ea79
Atul <atulanand94@gmail.com>
2018-08-19 22:32:16 +0530

    register service worker done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('js is working');
+const check = () => {
+    if (!('serviceWorker' in navigator)) {
+        throw new Error('No Service Worker support!');
+    }
+    if (!('PushManager' in window)) {
+        throw new Error('No Push API Support!');
+    }
+}
+
+
+const registerServiceWorker = async () => {
+    const swRegistration = await navigator.serviceWorker.register('service.js');
+    return swRegistration;
+}
+
+
+const main = async () => {
+    check();
+    const swRegistration = await registerServiceWorker();
+}
+
+main();
