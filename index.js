4a4118dc3e11c1abd4f55edda4970ff8525b5ce1
Atul <atulanand94@gmail.com>
2018-08-20 00:14:12 +0530

    Added notification permission

--- index.js
+++ index.js
@@ -13,10 +13,21 @@ const registerServiceWorker = async () => {
     return swRegistration;
 }
 
+const requestNotificationPermission = async () => {
+    const permission = await window.Notification.requestPermission();
+    // value of permission can be 'granted', 'default', 'denied'
+    // granted: user has accepted the request
+    // default: user has dismissed the notification permission popup by clicking on x
+    // denied: user has denied the request.
+    if(permission !== 'granted'){
+        throw new Error('Permission not granted for Notification');
+    }
+}
 
 const main = async () => {
     check();
     const swRegistration = await registerServiceWorker();
+    const permission =  await requestNotificationPermission();
 }
 
 main();
