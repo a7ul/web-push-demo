a676d62804a46227d9c788fbd67ba716b1bd1d43
Atul <atulanand94@gmail.com>
2018-09-18 00:05:55 +0200

    Successful subscribe

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
 const check = () => {
-    if (!('serviceWorker' in navigator)) {
-        throw new Error('No Service Worker support!');
-    }
-    if (!('PushManager' in window)) {
-        throw new Error('No Push API Support!');
-    }
-}
-
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No Service Worker support!");
+  }
+  if (!("PushManager" in window)) {
+    throw new Error("No Push API Support!");
+  }
+};
 
 const registerServiceWorker = async () => {
-    const swRegistration = await navigator.serviceWorker.register('service.js');
-    return swRegistration;
-}
+  const swRegistration = await navigator.serviceWorker.register("service.js");
+  return swRegistration;
+};
 
 const requestNotificationPermission = async () => {
-    const permission = await window.Notification.requestPermission();
-    // value of permission can be 'granted', 'default', 'denied'
-    // granted: user has accepted the request
-    // default: user has dismissed the notification permission popup by clicking on x
-    // denied: user has denied the request.
-    if(permission !== 'granted'){
-        throw new Error('Permission not granted for Notification');
-    }
-}
-
-const showLocalNotification = (title, body, swRegistration) => {
-    const options = {
-        body
-    };
-    swRegistration.showNotification(title, options)
-
-}
+  const permission = await window.Notification.requestPermission();
+  // value of permission can be 'granted', 'default', 'denied'
+  // granted: user has accepted the request
+  // default: user has dismissed the notification permission popup by clicking on x
+  // denied: user has denied the request.
+  if (permission !== "granted") {
+    throw new Error("Permission not granted for Notification");
+  }
+};
 
 const main = async () => {
-    check();
-    const swRegistration = await registerServiceWorker();
-    const permission =  await requestNotificationPermission();
-    showLocalNotification('This is title', 'this is the message', swRegistration);
-}
-
-main();
+  check();
+  const swRegistration = await registerServiceWorker();
+  const permission = await requestNotificationPermission();
+};
+// main(); we will not call main in the beginning.
--- service.js
+++ service.js
@@ -1,1 +1,26 @@
-console.log("Hello from service worker");
+const urlB64ToUint8Array = base64String => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
+
+self.addEventListener("install", async () => {
+  // This will be called only once when the service worker is installed for first time.
+  try {
+    const applicationServerKey = urlB64ToUint8Array(
+      "BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk"
+    );
+    const options = { applicationServerKey, userVisibleOnly: true };
+    const subscription = await self.registration.pushManager.subscribe(options);
+    console.log(JSON.stringify(subscription));
+  } catch (err) {
+    console.log("Error", err);
+  }
+});
